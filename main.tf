71643efeaf360f847eacc14ec8f2bdb941da6dc8
kitodorov <walou4walou@gmail.com>
2025-05-22 17:33:59 +0300

    add opentofu remote

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.AZURE_SUBSCRIPTION_ID
+  client_id       = var.AZURE_APP_ID
+  client_secret   = var.AZURE_APP_ID_SECRET
+  tenant_id       = var.AZURE_TENANT_ID
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+  tags = {
+    environment = "testing"
+    owner = "kiril"
+    created_by = "terraform_opentofu"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "AZURE_SUBSCRIPTION_ID" {}
+variable "AZURE_APP_ID" {}
+variable "AZURE_APP_ID_SECRET" {}
+variable "AZURE_TENANT_ID" {}
